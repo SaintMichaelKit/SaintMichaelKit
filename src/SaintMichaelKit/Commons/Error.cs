b2e68d30d6298a8a77d822da17f4ca0d169f1b69
AlarconVinicius <alarcon.pqd74@gmail.com>
2025-06-05 08:31:49 -0300

    feat: add Error classes

---
+++ src/SaintMichaelKit/Commons/Error.cs
@@ -0,0 +1,65 @@
+﻿namespace SaintMichaelKit.Commons;
+
+public record Error
+{
+    public string Code { get; }
+    public string Message { get; }
+    public ErrorType Type { get; }
+
+    protected Error(string code, string message, ErrorType type)
+    {
+        Code = code ?? throw new ArgumentNullException(nameof(code));
+        Message = message ?? throw new ArgumentNullException(nameof(message));
+        Type = type;
+    }
+
+    public static Error NotFound(string message) =>
+        new("NOT_FOUND", message, ErrorType.NotFound);
+
+    public static Error NotFound(string code, string message) =>
+        new(code, message, ErrorType.NotFound);
+
+    public static Error Validation(string message) =>
+        new("VALIDATION_ERROR", message, ErrorType.Validation);
+
+    public static Error Validation(string code, string message) =>
+        new(code, message, ErrorType.Validation);
+
+    public static Error Conflict(string message) =>
+        new("CONFLICT_ERROR", message, ErrorType.Conflict);
+
+    public static Error Conflict(string code, string message) =>
+        new(code, message, ErrorType.Conflict);
+
+    public static Error Failure(string message) =>
+        new("INTERNAL_SERVER_ERROR", message, ErrorType.Failure);
+
+    public static Error Failure(string code, string message) =>
+        new(code, message, ErrorType.Failure);
+
+    public static Error Forbidden(string message) =>
+        new("FORBIDDEN", message, ErrorType.Forbidden);
+
+    public static Error Forbidden(string code, string message) =>
+        new(code, message, ErrorType.Forbidden);
+
+    public static Error Unauthorized(string message) =>
+        new("UNAUTHORIZED", message, ErrorType.Unauthorized);
+
+    public static Error Unauthorized(string code, string message) =>
+        new(code, message, ErrorType.Unauthorized);
+
+    public static Error Problem(string message) =>
+        new("PROBLEM", message, ErrorType.Problem);
+
+    public static Error Problem(string code, string message) =>
+        new(code, message, ErrorType.Problem);
+
+    public static readonly Error None = new(string.Empty, string.Empty, ErrorType.Failure);
+
+    public static readonly Error NullValue = new("NULL_VALUE", "Null value was provided.", ErrorType.Failure);
+
+    public static readonly Error ConditionNotMet = new("CONDITION_NOT_MET", "The specified condition was not met.", ErrorType.Failure);
+
+    public override string ToString() => $"[{Type}] {Code}: {Message}";
+}
